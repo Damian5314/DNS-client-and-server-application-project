df1b2e64e83ff7d9fabeaf4cddedb1fa40ec6b7c
Wishant010 <wishantbhajan@outlook.com>
2025-04-04 13:15:54 +0200

    ww

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d538034138730caab3e2b55631cbf41306679f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b6dea37ab4286188d956baea234543a5ae5c53")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
